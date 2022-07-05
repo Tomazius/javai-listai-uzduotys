b5b28b8dcf63dc85a9f6f9f7a4ab1dd4ca722efa
Tomaszius <liutkus.to@gmail.com>
2022-07-05 12:14:15 +0300

    pabaigta 7 uzduotis

---
+++ src/uzduotis7.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class uzduotis7 {
+    public static void main(String[] args) {
+
+        int x = 258;
+
+
+        ArrayList<Integer> skaitmenai = skaitmenys(x);
+        System.out.println(sutvarkyti(skaitmenai));
+    }
+    public static ArrayList<Integer> skaitmenys(int x){
+        ArrayList<Integer> skaitmenai = new ArrayList<>();
+        while (x / 10 > 0){
+            skaitmenai.add(x % 10);
+            System.out.println(skaitmenai);
+
+            x = x / 10;
+            skaitmenai.add(x % 10);
+            System.out.println(skaitmenai);
+
+            x = x / 10;
+            skaitmenai.add(x % 10);
+            System.out.println(skaitmenai);
+        }
+        return skaitmenai;
+    }
+    public static ArrayList<Integer> sutvarkyti(ArrayList<Integer> skaitmenai){
+        Collections.reverse(skaitmenai);
+        return skaitmenai;
+    }
+
+
+    /**public static String skaitmenys(int x){
+        String pakaitalas = Integer.toString(x);
+        return pakaitalas;
+    }
+
+    public static int[] pakeistiIintList(String skaiciaiTekstu){
+        ArrayList<Integer> sarasas = new ArrayList<>();
+        for (int i = 0; i < skaiciaiTekstu.length(); i++) {
+            skaiciaiTekstu.get(1);
+        }
+    }*/
+}
